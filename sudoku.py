314298ae060be4bc219c7bc9be7a3c495c64b4cb
Ricardo Branco <rbranco@suse.de>
2021-03-21 18:57:54 +0100

    Solve easy puzzles

---
+++ sudoku.py
@@ -0,0 +1,223 @@
+"""Sudoku"""
+
+import sys
+import time
+from shutil import get_terminal_size
+
+
+FULL = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+
+class Sudoku:
+    """Sudoku"""
+    def __init__(self, digits="0" * 81):
+        digits = "".join(map(str, digits))
+        if len(digits) == 81 and digits.isdigit():
+            self.grid = [[int(digits[row * 9 + col]) for col in range(9)] for row in range(9)]
+        else:
+            raise ValueError("Invalid table")
+
+    def __str__(self):
+        return "".join(str(self.grid[row][col]) for row in range(9) for col in range(9))
+
+    def get_row(self, row):
+        "Get row"
+        return tuple(self.grid[row])
+
+    def get_col(self, col):
+        "Get column"
+        return tuple(self.grid[i][col] for i in range(9))
+
+    def get_diag(self, diag):
+        "Get diagonal"
+        if diag not in {0, 1}:
+            raise ValueError
+        if diag == 0:
+            return tuple(self.grid[num][num] for num in range(9))
+        return tuple(self.grid[num][8 - num] for num in range(9))
+
+    def get_box(self, row, col):
+        "Return a set with the numbers in the row"
+        x, y = 3 * (row // 3), 3 * (col // 3)
+        return tuple(self.grid[x + i][y + j] for i in range(3) for j in range(3))
+
+    def set_row(self, row, digits):
+        "Set row"
+        if len(digits) != 9:
+            raise ValueError
+        self.grid[row] = list(map(int, digits))
+
+    def set_col(self, col, digits):
+        "Set column"
+        if len(digits) != 9:
+            raise ValueError
+        for row in range(9):
+            self.grid[row][col] = int(digits[row])
+
+    def set_diag(self, diag, digits):
+        "Set diagonal"
+        if len(digits) != 9 or diag not in {0, 1}:
+            raise ValueError
+        if diag == 0:
+            for num in range(9):
+                self.grid[num][num] = int(digits[num])
+        else:
+            for num in range(9):
+                self.grid[num][8 - num] = int(digits[num])
+
+    def set_box(self, row, col, digits):
+        "Set box"
+        if len(digits) != 9:
+            raise ValueError
+        x, y = 3 * (row // 3), 3 * (col // 3)
+        for i in range(3):
+            for j in range(3):
+                self.grid[x + i][y + j] = int(digits[i * 3 + j])
+
+    def scan_row(self, row):
+        "Scan row for zeroes"
+        for col in range(9):
+            if self.grid[row][col] == 0:
+                yield col
+
+    def scan_col(self, col):
+        "Scan column for zeroes"
+        for row in range(9):
+            if self.grid[row][col] == 0:
+                yield row
+
+    def scan(self):
+        "Scan grid for zeroes"
+        for row in range(9):
+            for col in range(9):
+                if self.grid[row][col] == 0:
+                    yield row, col
+
+    def check_box(self, row, col):
+        "Check box"
+        box = self.get_box(row, col)
+        missing = FULL - set(box)
+        if len(missing) == 1:
+            x, y = 3 * (row // 3), 3 * (col // 3)
+            index = box.index(0)
+            i, j = index // 3, index % 3
+            self.grid[x + i][y + j] = missing.pop()
+            return True
+        return False
+
+    def check_row(self, row):
+        "Check row"
+        solved = 0
+        try_digits = FULL - set(self.get_row(row))
+        maybe = {n: [] for n in try_digits}
+        for try_col in self.scan_row(row):
+            for num in try_digits - set(self.get_box(row, try_col)) - set(self.get_col(try_col)):
+                maybe[num].append(try_col)
+        for num in maybe:
+            if len(maybe[num]) == 1:
+                self.grid[row][maybe[num][0]] = num
+                solved += 1
+        return solved
+
+    def check_col(self, col):
+        "Check column"
+        solved = 0
+        try_digits = FULL - set(self.get_col(col))
+        maybe = {n: [] for n in try_digits}
+        for try_row in self.scan_col(col):
+            for num in try_digits - set(self.get_box(try_row, col)) - set(self.get_row(try_row)):
+                maybe[num].append(try_row)
+        for num in maybe:
+            if len(maybe[num]) == 1:
+                self.grid[maybe[num][0]][col] = num
+                solved += 1
+        return solved
+
+    def check_diags(self):
+        "Check diagonals"
+        solved = 0
+        diag = self.get_diag(0)
+        missing = FULL - set(diag)
+        if len(missing) == 1:
+            index = diag.index(0)
+            self.grid[index][index] = missing.pop()
+            solved += 1
+        diag = self.get_diag(1)
+        missing = FULL - set(diag)
+        if len(missing) == 1:
+            index = diag.index(0)
+            self.grid[index][8 - index] = missing.pop()
+            solved += 1
+        return solved
+
+    def solve1(self):
+        "Solve low-hanging fruit in rows, columns & boxes"
+        solved = 0
+        for row, col in self.scan():
+            if self.check_box(row, col):
+                solved += 1
+            if self.grid[row][col]:
+                continue
+            solved += self.check_row(row)
+            if self.grid[row][col]:
+                continue
+            solved += self.check_col(col)
+        solved += self.check_diags()
+        return solved
+
+    def solver(self, show=True, seconds=1):
+        "Solver"
+        if show:
+            sudoku.show()
+        if seconds:
+            time.sleep(seconds)
+        while self.solve1():
+            if show:
+                sudoku.show()
+            if seconds:
+                time.sleep(seconds)
+
+    def validate_row(self, row):
+        "Validate row"
+        return set(self.get_row(row)) == FULL
+
+    def validate_col(self, col):
+        "Validate column"
+        return set(self.get_col(col)) == FULL
+
+    def validate(self):
+        "Validate grid"
+        return all(self.validate_row(row) for row in range(9)) and \
+            all(self.validate_col(col) for col in range(9))
+
+    def display(self):
+        "Display grid"
+        string = "╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗\n"
+        for row in range(9):
+            string += "║ {} │ {} │ {} ║ {} │ {} │ {} ║ {} │ {} │ {} ║\n".format(*self.grid[row])
+            if row == 8:
+                string += "╚═══╧═══╧═══╩═══╧═══╧═══╩═══╧═══╧═══╝\n"
+            elif (row + 1) % 3 == 0:
+                string += "╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣\n"
+            else:
+                string += "╟───┼───┼───╫───┼───┼───╫───┼───┼───╢\n"
+        return string.replace("0", " ")
+
+    def show(self):
+        "Show grid"
+        string = self.display()
+        columns, lines = get_terminal_size()
+        spaces = " " * ((columns - 37) // 2)
+        lines = "\n" * ((lines - 19) // 2)
+        string = f"{spaces}{string}"
+        string = string.replace("\n", f"\n{spaces}")
+        print("\033c", lines, string, lines, sep="")
+
+
+if __name__ == "__main__":
+    EASY = "000401000004020500069050810602905108107806309000000000700104002000060000300000007"
+    MEDIUM = "096000008000800240208010009000637000013000000000125000701060004000500690064000001"
+    sudoku = Sudoku(EASY)
+    sudoku.solver()
+    if not sudoku.validate():
+        sys.exit(1)
---
+++ sudoku_test.py
@@ -0,0 +1,123 @@
+"Sudoku test"
+
+
+import random
+import unittest
+
+from sudoku import Sudoku
+
+
+DIGITS = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+VALID = "123456789456789123789123456231564897564897231897231564312645978645978312978312645"
+INVALID = VALID[:-1] + "1"
+
+INCOMPLETE = "123456789456789123789123456231564897564897231897231564312645978645978312978312640"
+INCOMPLETE_DISPLAY = """
+╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗
+║ 1 │ 2 │ 3 ║ 4 │ 5 │ 6 ║ 7 │ 8 │ 9 ║
+╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+║ 4 │ 5 │ 6 ║ 7 │ 8 │ 9 ║ 1 │ 2 │ 3 ║
+╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+║ 7 │ 8 │ 9 ║ 1 │ 2 │ 3 ║ 4 │ 5 │ 6 ║
+╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣
+║ 2 │ 3 │ 1 ║ 5 │ 6 │ 4 ║ 8 │ 9 │ 7 ║
+╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+║ 5 │ 6 │ 4 ║ 8 │ 9 │ 7 ║ 2 │ 3 │ 1 ║
+╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+║ 8 │ 9 │ 7 ║ 2 │ 3 │ 1 ║ 5 │ 6 │ 4 ║
+╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣
+║ 3 │ 1 │ 2 ║ 6 │ 4 │ 5 ║ 9 │ 7 │ 8 ║
+╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+║ 6 │ 4 │ 5 ║ 9 │ 7 │ 8 ║ 3 │ 1 │ 2 ║
+╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+║ 9 │ 7 │ 8 ║ 3 │ 1 │ 2 ║ 6 │ 4 │   ║
+╚═══╧═══╧═══╩═══╧═══╧═══╩═══╧═══╧═══╝
+"""
+
+EASY = "000401000004020500069050810602905108107806309000000000700104002000060000300000007"
+EASY_SOLVED = "573481296814629573269357814632975148157846329498213765786134952925768431341592687"
+
+
+def random_digits():
+    "Return 9 random digits from 1..9 except one random replaced by 0"
+    digits = list(DIGITS)
+    random.shuffle(digits)
+    return "".join(map(str, digits)).replace(str(random.choice(digits)), "0")
+
+
+class Test_Sudoku(unittest.TestCase):
+    "Test Sudoku"
+
+    def test_validate1(self):
+        "Test valid Sudoku"
+        sudoku = Sudoku(VALID)
+        self.assertTrue(sudoku.validate())
+
+    def test_validate2(self):
+        "Test invalid Sudoku"
+        sudoku = Sudoku(INVALID)
+        self.assertFalse(sudoku.validate())
+
+    def test_grid(self):
+        "Test grid"
+        sudoku = Sudoku(VALID)
+        grid = [[int(VALID[row * 9 + col]) for col in range(9)] for row in range(9)]
+        self.assertEqual(sudoku.grid, grid)
+
+    def test_exception(self):
+        "Test exception"
+        with self.assertRaises(ValueError):
+            _ = Sudoku("1")
+            _ = Sudoku(1)
+            _ = Sudoku("blabla")
+
+    def test_display(self):
+        "Test display"
+        sudoku = Sudoku(INCOMPLETE)
+        self.assertEqual(sudoku.display(), INCOMPLETE_DISPLAY.lstrip())
+
+    def test_str(self):
+        "Test normalize"
+        sudoku = Sudoku(VALID)
+        self.assertEqual(str(sudoku), VALID)
+
+    def test_check_row(self):
+        "Test check_row()"
+        for row in range(9):
+            sudoku = Sudoku()
+            sudoku.set_row(row, random_digits())
+            sudoku.check_row(row)
+            self.assertEqual(set(sudoku.get_row(row)), DIGITS)
+
+    def test_check_col(self):
+        "Test check_col()"
+        for col in range(9):
+            sudoku = Sudoku()
+            sudoku.set_col(col, random_digits())
+            sudoku.check_col(col)
+            self.assertEqual(set(sudoku.get_col(col)), DIGITS)
+
+    def test_check_diag(self):
+        "Test check_diag()"
+        for diag in range(2):
+            sudoku = Sudoku()
+            sudoku.set_diag(diag, random_digits())
+            sudoku.check_diags()
+            self.assertEqual(set(sudoku.get_diag(diag)), DIGITS)
+
+    def test_check_box(self):
+        "Test check box"
+        for row in range(9):
+            for col in range(9):
+                sudoku = Sudoku()
+                sudoku.set_box(row, col, random_digits())
+                self.assertTrue(sudoku.check_box(row, col))
+                self.assertEqual(set(sudoku.get_box(row, col)), DIGITS)
+
+    def test_solver(self):
+        "Test solver"
+        sudoku = Sudoku(EASY)
+        sudoku.solver(show=False, seconds=0)
+        self.assertEqual(str(sudoku), EASY_SOLVED)
+        self.assertTrue(sudoku.validate())
